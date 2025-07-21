d2199b32980f1b7a0173be8d0524cd9b56be3b46
Urmit Kaundal <urmitkaundal467@gmail.com>
2025-07-21 13:12:35 +0530

    Update App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import EventManagement from "./assets/components/pages/EventManagement";
 import Auth from "./assets/components/pages/Auth";
